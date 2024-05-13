e9b2bf88eff8c95ccf571d1e96a3eb68b7a8672c
Zachary Beyer <zack@boomlearning.com>
2024-05-13 17:15:37 -0600

    updated complexity

--- helloWorld.js
+++ helloWorld.js
@@ -1,3 +1,17 @@
-function helloWorld (){
-	  return 'Hello, World!';
+
+// anotherFile.js
+import { sayHello, pi } from './myModule.js';
+
+console.log(sayHello('World')); // Output: Hello, World!
+console.log(pi); // Output: 3.14159
+
+// function helloWorld (){
+// 	  return 'Hello, World!';
+// }
+
+class exampleClass {
+	constructor(){
+		this.name = "exampleClass";
+	}
 }
+console.log("loaded helloWorld.js");
---
+++ myModule.js
@@ -0,0 +1,6 @@
+// myModule.js
+export function sayHello(name) {
+	return `Hello, ${name}!`;
+}
+
+export const pi = 3.14159;
