42074e0485ce50362e08660297609270c318cf7f
Zachary Beyer <zack@boomlearning.com>
2024-05-13 17:28:15 -0600

    don't call things that aren't being imported

--- helloWorld.js
+++ helloWorld.js
@@ -2,8 +2,8 @@
 // anotherFile.js
 // import { sayHello, pi } from './myModule.js';
 
-console.log(sayHello('World')); // Output: Hello, World!
-console.log(pi); // Output: 3.14159
+// console.log(sayHello('World')); // Output: Hello, World!
+// console.log(pi); // Output: 3.14159
 
 // function helloWorld (){
 // 	  return 'Hello, World!';
